f64f26a7178a457964519225a9ea336881912665
Dreas21 <96311691+Dreas21@users.noreply.github.com>
2024-11-19 10:46:34 +0100

    Add files via upload

--- 1911/ESERCIZIO_2/Esercizio_2.java
+++ 1911/ESERCIZIO_2/Esercizio_2.java
@@ -1,9 +1,12 @@
 public class Esercizio_2 {
     public static void main(String[] args) {
+        // inizializziamo un vettore che ci permette di inserire tutti i giorni della settimana
         String[] giornisettimana = {"Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica"};
+        // ciclo FOR che ci permette di scorrere lungo l'array
         for(int i=1; i<=giornisettimana.length; i++){
             switch (i) {
                 case 1:
+                // stampiamo giornisettimana(i-1) in modo che ci ritroviamo con l'indice corretto!
                 System.out.println("Il giorno della settimana: " + i + " - è: " + giornisettimana[i-1]);
                     break;
                 case 2:
--- 1911/ESERCIZIO_3/Esercizio_3.java
+++ 1911/ESERCIZIO_3/Esercizio_3.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 public class Esercizio_3{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // inseriamo il numero da controllare
         int numero;
         do{
             System.out.println("Inserisci numero: ");
             numero = scanner.nextInt();
+            // calcoliamo il resto della divisione per 2
             int resto = numero%2;
+            // se il resto della divisione per 2 sarà = 0 allora il numero cercato sarà pari, altrimenti no
             if(resto == 0){
                 System.out.println("Il numero inserito è pari|");
             } else {
                 System.out.println("Il numero inserito è dispari!");
             }
+        // continuiamo ad iterare fino a quando il numero inserito non sarà minore di 0
         }while (numero >= 0);
     }
 }
